1f98b0fe3215e21fb6141a33fa3ffa991134a4f0
jkurian49 <jkurian49@gmail.com>
2021-03-30 16:50:22 -0400

    call test script

--- test/gradio-test.py
+++ test/gradio-test.py
@@ -1,7 +1,13 @@
 import gradio as gr
+import os
+import shutil
 
-def greet(name):
-  return "Hello " + name + "! This program was run on Kahan :)"
+def test(audio):
+	filename = os.path.splitext(audio)[0]
+	shutil.move(audio,'test/raw_audio/')
+	return_code = subprocess.call("./test/test.sh")
+	video_path = "test/edited_output/"+filename+"_with_audio.mp4"
+	return video_path
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+iface = gr.Interface(fn=test, inputs="audio", outputs="video")
 iface.launch(share=True)
